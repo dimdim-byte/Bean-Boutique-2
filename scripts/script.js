5907abec5ffb249baf0bc94ec331c986e53a9b87
HtutAungWai110 <lmaohtutaungwai@gmail.com>
2025-11-03 00:50:53 +0630

    Merged chnages and major improvements

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById('registrationModal');
+  const openBtns = document.querySelectorAll('.register-btn');
+  const closeModalBtn = document.querySelector('.modal-close-btn');
+  const registrationForm = document.querySelector('.registration-form');
+
+  if (modal && openBtns && closeModalBtn && registrationForm){
+    openBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+          modal.style.display = 'flex';
+        });
+      });
+
+      closeModalBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+        
+      });
+
+      registrationForm.addEventListener('submit', (e)=> {
+        e.preventDefault();
+        modal.style.display = 'none';
+        showPopUp("Successfully registered")
+      })
+
+      window.addEventListener('click', (e) => {
+        if (e.target === modal) modal.style.display = 'none';
+      });
+  }
+
+ 
+})
+
 export const goToPage = (url) => {
   window.location.href = url;
 }
@@ -33,7 +65,7 @@ export const addToCart = (e) => {
         showPopUp(`${name} added to cart!`);
 }
 
-const showPopUp = (message) => {
+export const showPopUp = (message) => {
   
   const mainDiv = document.createElement('div');
   mainDiv.classList.add('popup-overlay');
